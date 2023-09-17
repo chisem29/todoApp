450938a3367cf976ddbde0c8ad976c0bdb2daca5
samChisem29 <tonyfhg1000@gmail.com>
2023-09-17 21:37:59 +0300

    пофиксили nice

--- frontend/src/components/UI/TodoList/TodoList.tsx
+++ frontend/src/components/UI/TodoList/TodoList.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 
-import { FormGroup, useMediaQuery } from "@mui/material";
+import { FormGroup } from "@mui/material";
 
 import useHandlerDeleteTask from "../../../hooks/useHandlerDeleteTask";
 import useHandlerToogleTask from "../../../hooks/useHandlerToogleTask";
@@ -20,43 +20,49 @@ const TodoList: FC = () => {
   const { indexVisible, handlerVisibleInner } = useVisibleInner(-1);
 
   return (
-    <ul className={styles.listTasks}>
-      {state.map(({ description, complete, name }, index) => (
-        <li
-          className={index === indexVisible ? styles.visibleItemAll : ""}
-          key={index}
-        >
-          <FormGroup className={styles.completeTask}>
-            <input
-              type="checkbox"
-              id="complete"
-              onChange={handlerToogleTask({ description, complete, name })}
-              defaultChecked={complete}
-            />
-            <label id="complete">
-              {" "}
-              <img src="https://img.icons8.com/?size=1x&id=8FZJBNvt3gGe&format=png" />
-            </label>
-          </FormGroup>
-          <div
-            onClick={handlerVisibleInner(
-              index === indexVisible ? state.length + 1 : index
-            )}
-            className={
-              styles.taskInner +
-              " " +
-              (index === indexVisible ? styles.visibleAll : "")
-            }
-          >
-            <h1>{name}</h1>
-            <p>{description}</p>
-          </div>
-          <button onClick={handlerDelete({ name })}>
-            <img src="https://img.icons8.com/?size=1x&id=68064&format=png" />
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      {!state.length ? (
+        <h4 className={styles.emptyMessage}> Empty. {"  "} Add tasks </h4>
+      ) : (
+        <ul className={styles.listTasks}>
+          {state.map(({ description, complete, name }, index) => (
+            <li
+              className={index === indexVisible ? styles.visibleItemAll : ""}
+              key={index}
+            >
+              <FormGroup className={styles.completeTask}>
+                <input
+                  type="checkbox"
+                  id="complete"
+                  onChange={handlerToogleTask({ description, complete, name })}
+                  defaultChecked={complete}
+                />
+                <label id="complete">
+                  {" "}
+                  <img src="https://img.icons8.com/?size=1x&id=8FZJBNvt3gGe&format=png" />
+                </label>
+              </FormGroup>
+              <div
+                onClick={handlerVisibleInner(
+                  index === indexVisible ? state.length + 1 : index
+                )}
+                className={
+                  styles.taskInner +
+                  " " +
+                  (index === indexVisible ? styles.visibleAll : "")
+                }
+              >
+                <h1>{name}</h1>
+                <p>{description}</p>
+              </div>
+              <button onClick={handlerDelete({ name })}>
+                <img src="https://img.icons8.com/?size=1x&id=68064&format=png" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
